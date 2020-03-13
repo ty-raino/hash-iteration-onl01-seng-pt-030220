917d38ee31d26079605b35d7c60a366c39b4159d
Ty Raino <mstymetris@yahoo.com>
2020-03-13 23:34:19 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+winnner = " "
+passengers.each do |suite, name|
+  if suite == :suite_a &&
+    name.start_with?("A")
+    winnner = name
+  end
 end
+
+winner
