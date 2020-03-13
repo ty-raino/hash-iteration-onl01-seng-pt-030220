cf2640e45f4830524ec4f27f59e3c2cf443e2d6f
Ty Raino <mstymetris@yahoo.com>
2020-03-13 23:58:04 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,10 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(greeting)
+  birthday_kids.each do |kids_name, age|
+    if age < 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+      puts "You are too old for this."
+    end
 end
